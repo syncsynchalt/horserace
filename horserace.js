57f752cde08217938a8dff0c84ad781295c435f4
Michael Driscoll <mdriscoll@pax8.com>
2017-02-18 16:46:52 -0700

    Get tooltips working

--- horserace.js
+++ horserace.js
@@ -66,10 +66,7 @@ var horseCount = 10,
             span.style.zIndex = getZIndex(i)+1;
             span.style.position = "fixed";
             span.style.left = pos.left;
-            span.style.top = pos.top + 40;
-            span.style.fontFamilty = 'fantasy, san-serif';
-            span.style.color = '#eee';
-            span.style.fontSize = '1.2em';
+            span.style.top = pos.top + 50;
             span.innerHTML = score;
             wave.appendChild(span);
         }
@@ -81,16 +78,40 @@ var horseCount = 10,
         }
     }());
 
+    function clearTooltips () {
+        var elements = document.getElementsByClassName('tooltip'), i;
+        for (i = 0; i < elements.length; i++) {
+            elements[i].parentNode.removeChild(elements[i]);
+        }
+    }
     function onHorseEnter () {
-        var me = this, tooltip;
-        console.log("moused " + me.style.left);
+        var horse = this,
+            num = horse.id.replace(/.*_/, ''),
+            horseLeft = parseInt(horse.style.left, 10),
+            tooltip = document.createElement('span'),
+            offset = Math.floor(150*window.innerWidth/140/8);
+        clearTooltips();
+        tooltip.id = 'tooltip_' + num;
+        tooltip.className = 'tooltip';
+        tooltip.style.position = 'fixed';
+        tooltip.style.top = parseInt(horse.style.top, 10) + waveHeight - 50;
+        if (horseLeft > window.innerWidth/2) {
+            tooltip.style.left = horseLeft-offset;
+        } else {
+            tooltip.style.left = horseLeft+offset;
+        }
+        tooltip.style.zIndex = parseInt(horse.style.zIndex, 10)+1;
+        tooltip.innerHTML = "Name: " + horseInfo[num].name + "<br>Score: " + horseInfo[num].score;
+        horse.parentNode.appendChild(tooltip);
     }
     function onHorseLeave () {
-        var me = this;
-        console.log("unmoused " + me.style.left);
+        var horse = this,
+            num = horse.id.replace(/.*_/, ''),
+            tooltip = document.getElementById('tooltip_' + num);
+        tooltip.parentNode.removeChild(tooltip);
     }
     (function addHorses () {
-        var i, img, pos, wave;
+        var i, img, pos, wave, scale;
         for (i = 1; i <= horseCount; i++) {
             wave = document.getElementById('wave_'+i);
             pos = getPosition(i, 0);
@@ -100,10 +121,13 @@ var horseCount = 10,
             img.style.zIndex = getZIndex(i)+2;
             img.style.position = 'fixed';
             img.style.left = pos.left-HORSE_LEFT_OFFSET;
-            img.style.top = pos.top-5;
+            img.style.top = pos.top-20;
             img.src = "horsey.png";
             img.onmouseover = onHorseEnter;
             img.onmouseleave = onHorseLeave;
+            scale = window.innerHeight/160/5;
+            img.height = Math.floor(160*scale);
+            img.width = Math.floor(140*scale);
             wave.appendChild(img);
         }
     }());
