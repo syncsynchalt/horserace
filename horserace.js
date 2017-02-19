5426ff93464e3a35d2351e62aafa1534098598ca
Michael Driscoll <mdriscoll@pax8.com>
2017-02-18 18:32:35 -0700

    Change tooltips from horse-hover to wave-hover

--- horserace.js
+++ horserace.js
@@ -35,8 +35,13 @@ var horseCount = 10,
         return 100*(OFFSET+horse);
     }
 
+    function updateTooltip (horse) {
+        var tooltip = document.getElementById('tooltip_' + horse);
+        tooltip.innerHTML = "Name: " + horseInfo[horse].name + "<br>Score: " + horseInfo[horse].score;
+    }
+
     (function makeWaves () {
-        var div, i, pos;
+        var div, i, pos, tooltip;
 
         for (i = 1; i <= horseCount; i++) {
             div = document.createElement('div');
@@ -51,7 +56,17 @@ var horseCount = 10,
             div.style.backgroundImage = 'url("sine.png")';
             div.style.backgroundSize = '100px 160px';
             div.style.backgroundRepeat = 'repeat-x';
+
+            tooltip = document.createElement('span');
+            tooltip.id = 'tooltip_' + i;
+            tooltip.className = 'tooltip';
+            tooltip.style.position = 'fixed';
+            tooltip.style.top = parseInt(div.style.top, 10) + waveHeight - 50;
+            tooltip.style.left = 100;
+            tooltip.style.zIndex = getZIndex(i)+3;
+            div.appendChild(tooltip);
             field.appendChild(div);
+            updateTooltip(i);
         }
     }());
 
@@ -79,40 +94,6 @@ var horseCount = 10,
         }
     }());
 
-    function clearTooltips () {
-        var elements = document.getElementsByClassName('tooltip'), i;
-        for (i = 0; i < elements.length; i++) {
-            elements[i].parentNode.removeChild(elements[i]);
-        }
-    }
-    function onHorseEnter () {
-        var horse = this,
-            num = horse.id.replace(/.*_/, ''),
-            horseLeft = parseInt(horse.style.left, 10),
-            tooltip = document.createElement('span'),
-            offset = Math.floor(150*window.innerWidth/140/8);
-        clearTooltips();
-        tooltip.id = 'tooltip_' + num;
-        tooltip.className = 'tooltip';
-        tooltip.style.position = 'fixed';
-        tooltip.style.top = parseInt(horse.style.top, 10) + waveHeight - 50;
-        if (horseLeft > window.innerWidth/2) {
-            tooltip.style.left = horseLeft-offset;
-        } else {
-            tooltip.style.left = horseLeft+offset;
-        }
-        tooltip.style.opacity = 0;
-        tooltip.style.zIndex = parseInt(horse.style.zIndex, 10)+1;
-        tooltip.innerHTML = "Name: " + horseInfo[num].name + "<br>Score: " + horseInfo[num].score;
-        horse.parentNode.appendChild(tooltip);
-        window.setTimeout(function() { tooltip.style.opacity = 1 }, 1);
-    }
-    function onHorseLeave () {
-        var horse = this,
-            num = horse.id.replace(/.*_/, ''),
-            tooltip = document.getElementById('tooltip_' + num);
-        tooltip.parentNode.removeChild(tooltip);
-    }
     (function addHorses () {
         var i, img, pos, wave, scale;
         for (i = 1; i <= horseCount; i++) {
@@ -126,8 +107,6 @@ var horseCount = 10,
             img.style.left = pos.left-HORSE_LEFT_OFFSET;
             img.style.top = pos.top-20;
             img.src = "horsey.png";
-            img.onmouseover = onHorseEnter;
-            img.onmouseleave = onHorseLeave;
             scale = window.innerHeight/160/5;
             img.height = Math.floor(160*scale);
             img.width = Math.floor(140*scale);
@@ -135,10 +114,11 @@ var horseCount = 10,
         }
     }());
 
-    function updateHorseScore (horse, score) {
-        var horse = document.getElementById('horse_'+horse),
+    function updateHorseScore (horseNum, score) {
+        var horse = document.getElementById('horse_'+horseNum),
             pos = getPosition(horse, score);
         horse.style.left = pos.left-HORSE_LEFT_OFFSET;
+        updateTooltip(horseNum);
     };
 
     (function simulateRace () {
