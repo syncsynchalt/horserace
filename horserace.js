0d83b43ca6450c12721b19c49ad47bb09760b777
Michael Driscoll <mdriscoll@pax8.com>
2017-02-22 22:37:43 -0700

    WIP: Extract wave adder, change scoremark visibility from code to css

--- horserace.js
+++ horserace.js
@@ -1,5 +1,5 @@
 var horseCount = 10,
-    maxPoints = 45;
+    maxPoints = 48;
 
 (function app () {
     var HORSE_LEFT_OFFSET = 80,
@@ -31,75 +31,76 @@ var horseCount = 10,
         }
     }
 
-    function getZIndex(horse) {
-        return 100*(OFFSET+horse);
-    }
-
     function updateTooltip (horse) {
         var tooltip = document.getElementById('tooltip_' + horse);
         tooltip.innerHTML = "Name: " + horseInfo[horse].name + "<br>Score: " + horseInfo[horse].score;
     }
 
+    function calculatePositions() {
+    }
+    function addWave (horseNum) {
+        var wave, scoremark, pos;
+        pos = getPosition(horseNum, 0);
+
+        wave = document.createElement('div');
+        wave.className = 'wave';
+        wave.id = 'wave_'+horseNum;
+        wave.style.width = '400%';
+        wave.style.height = '160px';
+        wave.style.position = "fixed";
+        wave.style.left = 0 - 20*horseNum;
+        wave.style.top = pos.top;
+        wave.style.backgroundImage = 'url("sine.png")';
+        wave.style.backgroundSize = '100px 160px';
+        wave.style.backgroundRepeat = 'repeat-x';
+        wave.ontouchstart = function() {this.className = 'wave touching'};
+        wave.ontouchend = function() {this.className = 'wave'};
+
+        for (scoremark = 0; scoremark <= maxPoints; scoremark += 5) {
+            markpos = getPosition(horseNum, scoremark);
+            mark = document.createElement('span');
+            mark.className = 'scoremark';
+            mark.style.position = "fixed";
+            mark.style.left = markpos.left;
+            mark.style.top = markpos.top + 50;
+            mark.innerHTML = scoremark;
+            wave.appendChild(mark);
+        }
+
+        horse = document.createElement('img');
+        horse.id = 'horse_'+horseNum;
+        horse.className = 'horse';
+        horse.style.position = 'fixed';
+        horse.style.left = pos.left-HORSE_LEFT_OFFSET;
+        horse.style.top = pos.top-20;
+        horse.src = "placeholder.png";
+        scale = window.innerHeight/160/5;
+        horse.height = Math.floor(160*scale);
+        horse.width = Math.floor(140*scale);
+        wave.appendChild(horse);
+
+        tooltip = document.createElement('span');
+        tooltip.id = 'tooltip_' + horseNum;
+        tooltip.className = 'tooltip';
+        tooltip.style.position = 'fixed';
+        tooltip.style.top = parseInt(wave.style.top, 10) + waveHeight - 80;
+        tooltip.style.left = 100;
+        tooltip.style.zIndex = 1;
+        wave.appendChild(tooltip);
+
+        field.appendChild(wave);
+        updateTooltip(horseNum);
+        calculatePositions();
+    }
+    function removeWave (horseNum) {
+        var wave = document.getElementById('wave_' + horseNum);
+        wave.parentNode.remove(wave);
+        calculatePositions();
+    }
     (function makeWaves () {
-        var wave, horse, i, mark, scoremark, markpos, pos, tooltip;
-
-        var waveTouchStart = function() {
-            this.className = 'wave touching';
-        }
-        var waveTouchEnd = function() {
-            this.className = 'wave';
-        }
+        var i;
         for (i = 1; i <= horseCount; i++) {
-            pos = getPosition(i, 0);
-
-            wave = document.createElement('div');
-            wave.className = 'wave';
-            wave.id = 'wave_'+i;
-            wave.style.width = '400%';
-            wave.style.height = '160px';
-            wave.style.position = "fixed";
-            wave.style.left = 0 - 20*i;
-            wave.style.top = pos.top;
-            wave.style.backgroundImage = 'url("sine.png")';
-            wave.style.backgroundSize = '100px 160px';
-            wave.style.backgroundRepeat = 'repeat-x';
-            wave.ontouchstart = waveTouchStart;
-            wave.ontouchend = waveTouchEnd;
-
-            for (scoremark = 0; i%3 === 1 && scoremark <= maxPoints; scoremark += 6) {
-                markpos = getPosition(i, scoremark);
-                mark = document.createElement('span');
-                mark.className = 'scoremark';
-                mark.style.position = "fixed";
-                mark.style.left = markpos.left;
-                mark.style.top = markpos.top + 50;
-                mark.innerHTML = scoremark;
-                wave.appendChild(mark);
-            }
-
-            horse = document.createElement('img');
-            horse.id = 'horse_'+i;
-            horse.className = 'horse';
-            horse.style.position = 'fixed';
-            horse.style.left = pos.left-HORSE_LEFT_OFFSET;
-            horse.style.top = pos.top-20;
-            horse.src = "horsey.png";
-            scale = window.innerHeight/160/5;
-            horse.height = Math.floor(160*scale);
-            horse.width = Math.floor(140*scale);
-            wave.appendChild(horse);
-
-            tooltip = document.createElement('span');
-            tooltip.id = 'tooltip_' + i;
-            tooltip.className = 'tooltip';
-            tooltip.style.position = 'fixed';
-            tooltip.style.top = parseInt(wave.style.top, 10) + waveHeight - 80;
-            tooltip.style.left = 100;
-            tooltip.style.zIndex = 1;
-            wave.appendChild(tooltip);
-
-            field.appendChild(wave);
-            updateTooltip(i);
+            addWave(i);
         }
     }());
 
